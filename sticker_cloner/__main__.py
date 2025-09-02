029fd2e135cd1b7d99086e715056ac717ae14bef
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-09-02 03:11:10 +0200

    uncomment vital line

--- sticker_cloner/__main__.py
+++ sticker_cloner/__main__.py
@@ -32,7 +32,7 @@ def scanner_loop():
         img = po.adjust_image(img)
         img = crop_image(config.get("scanner",{}),img)
         img.show("Processed Image")
-        #po.print_image(img,1)
+        po.print_image(img,1)
 if __name__ == "__main__":
     if len(argv) == 2:
         scanner_loop()
